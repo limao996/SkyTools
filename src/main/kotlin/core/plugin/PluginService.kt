f97770357fb88883e2582fa5ad52e8a043c1f7cc
狸猫呐 <762259384@qq.com>
2024-01-18 16:36:24 +0800

    feat(PluginService): 日常更新

--- src/main/kotlin/core/plugin/PluginService.kt
+++ src/main/kotlin/core/plugin/PluginService.kt
@@ -1,6 +1,28 @@
 package core.plugin
 
+import config.UIConfig
 import core.Service
+import manager.core.ThemeManager
+import java.io.File
 
 abstract class PluginService : Service {
+	lateinit var config: PluginConfig
+	val dataDir = File("assets/plugin/" + config.packageName)
+
+	init {
+		dataDir.mkdirs()
+	}
+
+	fun setThemeMode(mode: ThemeMode) {
+		ThemeManager.current = when (mode) {
+			ThemeMode.System -> ThemeManager.Light
+			ThemeMode.Light -> ThemeManager.Dark
+			ThemeMode.Dark -> ThemeManager.System
+		}
+		UIConfig.theme = ThemeManager.current
+	}
+
+	enum class ThemeMode {
+		Light, Dark, System
+	}
 }
